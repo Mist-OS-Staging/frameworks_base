0bdc348de2e07e1205ccace92308c6df08fb5dc4
Pranav Vashi <neobuddy89@gmail.com>
2023-04-01 13:30:59 +0530

    Doze-on-charge: Add few improvements and fixes
    * Update detection logic when device is plugged in and is charging.
    * Fix issue in PowerManagerService - mIsPowered is updated dynamically and
      so it should be checked everytime, not just in init.
    * When plugging out, let device wake up. This should fix odd blinking issues
      in some devices.
    * Add DOZE_ON_CHARGE to content observer, duh.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/LensScreenshotReceiver.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LensScreenshotReceiver.java
@@ -31,7 +31,7 @@ import android.net.Uri;
 import android.os.Process;
 import android.os.UserHandle;
 
-import com.android.internal.util.lunaris.Utils;
+import com.android.internal.util.mist.Utils;
 
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.res.R;
