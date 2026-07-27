8de5f882dda23a8ea5e0b3b2ee7a0f82587ebbbd
rituj <ritujbeniwal@gmail.com>
2026-07-27 07:13:29 +0000

    base: Add vibration patterns from OOS [1/3] idoybh edits: Add settings value to backup array
    Change-Id: I9622c17a1969598af71bcee573a576b752b96c5e
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6943,6 +6943,18 @@ public final class Settings {
         @Readable
         public static final String FLASHLIGHT_ON_CALL_RATE = "flashlight_on_call_rate";
 
+         /**
+         * Which Vibration Pattern to use
+         * 0: dzzz-dzzz
+         * 1: dzzz-da
+         * 2: mm-mm-mm
+         * 3: da-da-dzzz
+         * 4: da-dzzz-da
+         * @hide
+         */
+        @Readable
+        public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+
         /**
          * Navbar style
          * @hide
