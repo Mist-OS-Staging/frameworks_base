cce6e4fbd76fe2fe2f68b1c30c48c9c3ba670433
pbzin <pbzinwindows@gmail.com>
2026-08-28 22:03:50 -0300

    services: Apply DeviceConfig defaults before consumers start
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2273,6 +2273,12 @@ public final class SystemServer implements Dumpable {
             }
             t.traceEnd();
 
+            // CustomDeviceConfigService must run before services that read boot-time
+            // DeviceConfig flags.
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
+            t.traceEnd();
+
             t.traceBegin("StartNetworkManagementService");
             try {
                 networkManagement = NetworkManagementService.create(context);
@@ -2975,11 +2981,6 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
             }
-
-            // CustomDeviceConfigService
-            t.traceBegin("StartCustomDeviceConfigService");
-            mSystemServiceManager.startService(CustomDeviceConfigService.class);
-            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
